60cfb073b28957a148680dbb24581f50c849c750
tkmuller <edwardo.muller@gmail.com>
2024-09-09 18:08:23 -0300

    first commit

---
+++ src/main/java/com/twittercasero/middleend/MiddleendApplication.java
@@ -0,0 +1,13 @@
+package com.twittercasero.middleend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiddleendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiddleendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/twittercasero/middleend/MiddleendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twittercasero.middleend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiddleendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
